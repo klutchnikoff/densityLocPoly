f02885e729ffa39a971f59e4212dbba390df3d9d
karbertin <140178813+karbertin@users.noreply.github.com>
2023-07-21 20:15:33 +0200

    Update estimation_poly_sector.R
    update

--- examples/estimation_poly_sector.R
+++ examples/estimation_poly_sector.R
@@ -1,44 +1,53 @@
 ##
-## Initialization
+## Seed
 ##
-
-k <- 1
-eps <- 0.01
-domain <- polynomial_sector(k)
-at <- spatstat.geom::ppp(eps, eps^k/2, domain)
-
 
 set.seed(1234)
 M <- spatstat.random::runifpoint(1, win = domain)
 a <- M$x
 b <- M$y
+
+##
+## Initialization
+##
+
+k <- 1.6
+eps <- 0.01
+domain <- polynomial_sector(k)
+at <- spatstat.geom::ppp(eps, eps^k/2, domain)
+
+
 f0 <-  function(x, y) {
   g <- function(u, v) {(u-a)**2 + (v-b)**2}
   A <- spatstat.geom::integral(spatstat.geom::as.im(g, domain), domain = domain)
   g(x,y)/A
 }
-data <- spatstat.random::rpoint(1000, f0, win = domain)
+
+f1 <- function(x,y) {
+  1 / spatstat.geom::integral(spatstat.geom::as.im(domain, domain))
+}
+
+f <- f0
+data <- spatstat.random::rpoint(1000, f, win = domain)
 
 
 ##
 ## Visualization
 ##
 
-par(mfrow = c(1,1))
 plot(domain, main = "Disque")
 points(data, pch = 20, col = "red")
 points(at, pch = 20, lwd = 10, col = "blue")
-points(M, pch = 20, lwd = 5, col = "green")
 
 ##
 ## Computations
 ##
-par(mfrow = c(1,1))
 
-bandwidth <- c(x = 0.07, y = 0.05)
+bandwidth <- c(x = 0.5, y = 0.5)
 degree <- 1
 fd <- density_estimation(data, domain, at_points=at, bandwidth=bandwidth, degree=degree)
 
-mean((fd[[1]] - f0(at$x, at$y))**2)
-f0(at$x, at$y)
-fd[[1]]
+mean((fd - f(at$x, at$y))**2)
+f(at$x, at$y)
+fd
+
